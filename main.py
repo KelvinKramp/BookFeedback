3cd01a57226ec13906029597c1c36ba739332f2e
Kelvin Kramp <k.h.kramp@gmail.com>
2021-11-03 12:34:05 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,257 @@
+# IMPORT MODULES
+import dash
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+import dash_core_components as dcc
+import dash_bootstrap_components as dbc
+from datetime import datetime as dt
+import subprocess
+
+try:
+    subprocess.run("lsof -t -i tcp:8080 | xargs kill -9", shell=False) # kill the server
+except Exception as e:
+    print(e)
+
+score = [{'label': 'Strongly agree', 'value': '1'}, {'label': 'Agree', 'value': '2'}, {'label': 'Neutral', 'value': '3'}, {'label': 'Disagree', 'value': '4'}, {'label': 'Strongly disagree', 'value': '5'}]
+style_score = {'display': 'inline-block', 'text-align': 'center', 'display': 'flex',
+                                           'justify-content': 'space-evenly', 'width': '30%'}
+labelStyle_score = {'display': 'inline-block', 'text-align': 'right','width': '100%',}
+
+introduction_text = """Several persons have read this book before you and given feedback that was incorporated in the text you've read. They have all sort of read the text from the perspective of an editor.
+                                        This probably caused them to read the book in a different reading style. I gave you a copy because you have contributed directly or indirectly to the writing or because you were just genuinely interested in what I've written down. 
+                                        I didn't tell you that it wasn't the final version because I wanted you to read as if it was the real thing. If you've had the chance to read it, I would love to hear your opinion about what you've read and use it in the final version before I
+                                        create public awareness on a larger scale."""
+open_question_1 = "What did you think was the most interesting chapter and why?"
+open_question_2 = "Did you get stuck/disinterested while reading? and if yes where and why?"
+open_question_3 = "If you think it was incomplete can you tell me what you think was missing?"
+open_question_4 = "Do you have any other comments?"
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
+server = app.server
+app.layout = html.Div(children=[
+                            dbc.Row([
+                                dbc.Col(html.H1("Feedback Book", style={'textAlign': 'center'}),
+                                        width=12)
+                            ]),
+                            dbc.Row([
+                                dbc.Col(html.H2("Urban kiz: a new vision on partner dance", style={'textAlign': 'center'}),
+                                        width=12)
+                            ]),
+                            dbc.Row([
+                                html.H3(" ", style={"display": "inline-block", 'textAlign': 'center', "width": "100%", }),
+                            ]),
+                            dbc.Row([
+                                dbc.Col(html.H4(introduction_text, style= {'width': 'fit-content','overflow-wrap':'break-word'}),
+                                        width={"size": 10, "offset": 1},
+                                        )],),
+                            dbc.Row([
+                                html.H3(" ", style={"display": "inline-block", 'textAlign': 'center', "width": "100%", }),
+                            ]),
+                            html.Br(),
+                            dbc.Row([
+                                html.H3("Multiple choice questions",
+                                        style={"display": "inline-block", 'textAlign': 'center', "width": "100%", }),
+                            ]),
+                            dbc.Row(children=[
+                                        html.H5("The text was understandable",
+                                                 style={ "display": "inline-block", 'textAlign': 'center', "width": "50%",}),
+                                        dcc.RadioItems(
+                                            options=score,
+                                            id='1',
+                                            value='',
+                                            labelStyle=labelStyle_score,
+                                            style=style_score,
+                                        ),
+                            ],),
+                            dbc.Row(children=[
+                                        html.H5("The text had flow",
+                                                 style={ "display": "inline-block", 'textAlign': 'center', "width": "50%",}),
+                                        dcc.RadioItems(
+                                            options=score,
+                                            id='2',
+                                            value='',
+                                            labelStyle=labelStyle_score,
+                                            style=style_score,
+                                        ),
+                            ],),
+                            dbc.Row(children=[
+                                html.H5("The ideas were interesting",
+                                        style={"display": "inline-block", 'textAlign': 'center', "width": "50%", }),
+                                dcc.RadioItems(
+                                    options=score,
+                                    id='3',
+                                    value='',
+                                    labelStyle=labelStyle_score,
+                                    style=style_score,
+                                ),
+                            ], ),
+                            dbc.Row(children=[
+                                html.H5("The text was too long",
+                                        style={"display": "inline-block", 'textAlign': 'center', "width": "50%", }),
+                                dcc.RadioItems(
+                                    options=score,
+                                    id='4',
+                                    value='',
+                                    labelStyle=labelStyle_score,
+                                    style=style_score,
+                                ),
+                            ], ),
+                            dbc.Row(children=[
+                                html.H5("The book was complete",
+                                        style={"display": "inline-block", 'textAlign': 'center', "width": "50%", }),
+                                dcc.RadioItems(
+                                    options=score,
+                                    id='5',
+                                    value='',
+                                    labelStyle=labelStyle_score,
+                                    style=style_score,
+                                ),
+                            ], ),
+                            html.Br(),
+                            dbc.Row([
+                                html.H3("Open questions",
+                                        style={"display": "inline-block", 'textAlign': 'center', "width": "100%", }),
+                            ]),
+                            dbc.Row(children=[
+                                dbc.Col(
+                                html.H5(open_question_1,
+                                        style={"display": "inline-block", "width": "100%", },
+                                        ), width={"size": 10, "offset": 2},),]),
+                            dbc.Row(children=[
+                                dbc.Col(
+                                dbc.ModalBody(children=(dcc.Textarea(
+                                id='textarea1',
+                                value='',
+                                spellCheck=True,
+                                style={"display": "flex", 'justifyContent':'center','width': '80%', 'height': 100},
+                            ),),
+                            ),width={"size": 10, "offset": 2},),]),
+
+                            dbc.Row(children=[
+                                dbc.Col(
+                                    html.H5(open_question_2,
+                                            style={"display": "inline-block", "width": "100%", },
+                                            ), width={"size": 10, "offset": 2}, ), ]),
+                            dbc.Row(children=[
+                                dbc.Col(
+                                    dbc.ModalBody(children=(dcc.Textarea(
+                                        id='textarea2',
+                                        value='',
+                                        spellCheck=True,
+                                        style={"display": "flex", 'justifyContent': 'center', 'width': '80%', 'height': 100},
+                                    ),),
+                                    ), width={"size": 10, "offset": 2}, ), ]),
+
+                            dbc.Row(children=[
+                                dbc.Col(
+                                    html.H5(open_question_3,
+                                            style={"display": "inline-block", "width": "100%", },
+                                            ), width={"size": 10, "offset": 2}, ), ]),
+                            dbc.Row(children=[
+                                dbc.Col(
+                                    dbc.ModalBody(children=(dcc.Textarea(
+                                        id='textarea3',
+                                        value='',
+                                        spellCheck=True,
+                                        style={"display": "flex", 'justifyContent': 'center', 'width': '80%', 'height': 100},
+                                    ),),
+                                    ), width={"size": 10, "offset": 2}, ), ]),
+
+                            dbc.Row(children=[
+                                dbc.Col(
+                                    html.H5(open_question_4,
+                                            style={"display": "inline-block", "width": "100%", },
+                                            ), width={"size": 10, "offset": 2}, ), ]),
+                            dbc.Row(children=[
+                                dbc.Col(
+                                    dbc.ModalBody(children=(dcc.Textarea(
+                                        id='textarea4',
+                                        value='',
+                                        spellCheck=True,
+                                        style={"display": "flex", 'justifyContent': 'center', 'width': '80%', 'height': 100},
+                                    ),),
+                                    ), width={"size": 10, "offset": 2}, ), ]),
+                            html.Br(),
+                            html.Div([
+                                dbc.Button("Submit", id="submit-button", className="ml-auto", n_clicks=0,style={ 'width': '150%'},
+                                            ),
+                            ], style={'margin-bottom': '10px',
+                                      'textAlign': 'center',
+                                      'width': '220px',
+                                      'margin': 'auto'}
+                            ),
+                            html.Br(),
+                            html.Div('',id='empty-div-output'),
+                            dbc.Modal(
+                                [
+                                    dbc.ModalHeader("Do you want to submit the feedback?"),
+                                    dbc.ModalFooter(children=[
+                                        dbc.Button("Save", id="save", className="ml-auto", n_clicks=0),
+                                    ]
+                                    ),
+                                ],
+                                is_open=False,
+                                id="modal",
+                                style={"white-space": "break-spaces"},
+                                backdrop=False
+                            ),
+                            dbc.Modal(
+                                [
+                                    dbc.ModalHeader("You have submitted. Thank you for giving your feedback"),
+                                    dbc.ModalFooter(children=[
+                                        dbc.Button("Close", id="close", className="ml-auto", n_clicks=0),
+                                    ]
+                                    ),
+                                ],
+                                is_open=False,
+                                id="modal2",
+                                style={"white-space": "break-spaces"},
+                                backdrop=False
+                            ),
+                            ])
+
+
+
+### CALLBACKS ## the numbers are LETTER O followed by a number
+@app.callback(
+    Output("modal", "is_open"),
+    Output("modal2", "is_open"),
+    [Input('1', 'value'),
+     Input('2', 'value'),
+     Input('3', 'value'),
+     Input('4', 'value'),
+     Input('5', 'value'),
+     Input('textarea1', 'value'),
+     Input('textarea2', 'value'),
+     Input('textarea3', 'value'),
+     Input('textarea4', 'value'),
+     Input('submit-button', 'n_clicks'),
+     Input('save', 'n_clicks'),
+     Input('close', 'n_clicks')],
+    [State("modal", "is_open"),
+     State("modal2", "is_open")],
+)
+def submit(Question1, Question2, Question3, Question4, Question5, text1, text2, text3, text4, submitclick, save, close, is_open, is_open2):
+    if save == 0 and submitclick== 1:
+        print("save value =", save)
+        print("submit value =", submitclick)
+        return not is_open, is_open2
+    elif submitclick >1:
+        print("already submitted")
+        return is_open, not is_open2
+    elif close:
+        print("closing window")
+        return not is_open, not is_open2
+    elif save == 1 and submitclick==1:
+        print("save value =", save)
+        print("submit value =", submitclick)
+        print(Question1, Question2, Question3, Question4, Question5, text1, text2, text3, text4)
+        print("all input saved")
+        return is_open, not is_open2
+    print("save value =", save)
+    print("submit value =",submitclick)
+    return is_open, is_open2
+
+
+if __name__ == '__main__':
+    app.run_server(host='0.0.0.0', port=8080, debug=True, use_reloader=False)
