03bb40524dd435a1a3f9ac11b0665b981934e6ee
Kelvin Kramp <k.h.kramp@gmail.com>
2021-11-11 02:51:55 +0100

    update

--- upload_heroku.sh
+++ upload_heroku.sh
@@ -3,4 +3,5 @@ pip freeze > requirements.txt
 git add .
 git commit -m "update"
 git push heroku master
+git push https://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@github.com/KelvinKramp/BookFeedback.git
 heroku logs -n 20
