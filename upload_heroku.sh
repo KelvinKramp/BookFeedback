7c7a9e81264ccdb8fc27d9bab972a090578aec5f
Kelvin Kramp <k.h.kramp@gmail.com>
2021-11-11 11:46:13 +0100

    update

--- upload_heroku.sh
+++ upload_heroku.sh
@@ -4,4 +4,4 @@ git add .
 git commit -m "update"
 git push https://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@github.com/KelvinKramp/BookFeedback.git
 #git push heroku master
-heroku logs -n 20
+#heroku logs -n 20
