e20c81898d049c2e0c8d38f2cd634d14fa2b7675
Kelvin Kramp <k.h.kramp@gmail.com>
2021-11-12 01:49:55 +0100

    Remove the now ignored files

--- upload_heroku.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-pip freeze > requirements.txt
-git add .
-git commit -m "update"
-git push https://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@github.com/KelvinKramp/BookFeedback.git
-git push heroku master
-heroku logs -n 20
