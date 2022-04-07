ef926af3d854a2dc08345fbb21f58c44aeedafaa
Kelvin Kramp <k.h.kramp@gmail.com>
2022-04-07 17:30:12 +0200

    update

--- get_feedback.py
+++ get_feedback.py
@@ -2,28 +2,26 @@ import pandas as pd
 import os
 from dotenv import load_dotenv
 load_dotenv()
+from sqlalchemy import create_engine
 
 
 # MAKE DIFFERENCE BETWEEN PRODUCTION AND DEVELOPMENT ENVIRONMENT
-def conn_db():
-    from sqlalchemy import create_engine
-    SQL_URI = os.environ.get('DATABASE_URL')
+def conn_db(DEV=True):
+    if DEV:
+        SQL_URI = os.environ.get('SQL_URI')
+    else:
+        SQL_URI = os.environ.get('SQL_URI_HEROKU')
     if SQL_URI and SQL_URI.startswith("postgres://"):
         SQL_URI = SQL_URI.replace("postgres://", "postgresql://", 1)
-    con = create_engine(SQL_URI, echo=True).raw_connection()
+    con = create_engine(SQL_URI, echo=False).raw_connection()
     return con
 
 
-def get_from_db(section):
-    con = conn_db()
-
-    # DIFFERENT TABLE SELECTIONS
-    df = pd.read_sql('SELECT * FROM "{}";'.format(section), con)
-    df.to_csv("feedback.csv")
-    return
-
 
 if __name__ == "__main__":
+    DEV=False
     sections = ["feedback_book", "buy_hardcover", "report_bug"]
-    df = get_from_db()
+    con = conn_db(DEV)
+    query = 'SELECT * FROM "{}";'.format(sections[0])
+    df = pd.read_sql(query, con)
     print(df)
